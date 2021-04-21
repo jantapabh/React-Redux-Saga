2b322077c635e7edb05c31296026cc5eae4a7c55
devInternPir <devintern@pirsquare.net>
2021-04-21 12:49:08 +0700

    1

---
+++ src/components/CardComponent.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Card = (props) => {
+  return (
+    <div className="card">
+      <div className="card-body">
+        <div className="card-title">
+          <h6 className="card-subtitle mb-2 text-muted"></h6>
+          <p className="card-text"></p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/redux/store.js
@@ -0,0 +1,8 @@
+import { createStore, compose } from 'redux'
+import rootReducer from './reducers/index.js'
+
+const store = createStore(
+    rootReducer, /* preloadedState, */
+ +  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
+export default store;
